3ebee32539dea57679cf3f01ca3aa283ab0bdfd8
Andrew Gray <andrew@blackstream.com.au>
2022-07-13 10:39:16 +1000

    add check for command line params

--- nb.sh
+++ nb.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+if [[ $# -eq 0 ]] ; then
+    echo 'Error: Branch name is required.'
+    exit 1
+fi
+
 branch_name=$1
 git checkout -b $branch_name
 git push --set-upstream origin $branch_name
